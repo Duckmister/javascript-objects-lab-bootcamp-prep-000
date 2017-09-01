d606d5f590f25cec950cc30a8f1b1df6c2769942
T Hinder <theresa.hindersinn@gmail.com>
2017-09-01 15:55:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,15 +6,11 @@ function updateObjectWithKeyAndValue(object, key, value){
   return object, newObject
 }
 
-function updateObjectWithKeyAndValue(object, key, value){
-  return Object.assign( object, {key: value})
-}
-
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
-  object[key]= value
+  object[key]= value;
   return object
 }
-/*
+
 function deleteFromObjectByKey(object, key){
 
 }
